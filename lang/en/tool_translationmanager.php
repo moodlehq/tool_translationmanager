ad9f8e521d9de5c4be16a9dee13db7fd5edf7787
David Mudrák <david@moodle.com>
2021-02-04 19:20:36 +0100

    Fix copyright and php docs

--- lang/en/tool_translationmanager.php
+++ lang/en/tool_translationmanager.php
@@ -15,11 +15,10 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Lang import
+ * Strings provided by the plugin.
  *
- * @package    tool
- * @subpackage langimport
- * @copyright  2011 Petr Skoda
+ * @package    tool_translationmanager
+ * @copyright  2020 Farhan Karmali <farhan6318@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
